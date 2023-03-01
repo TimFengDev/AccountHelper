199c43fdc8435c97b636c24ea8eb45b1a7864ad4
TimFeng <timf3801@gmail.com>
2023-03-01 19:52:15 +0800

    產生圓圈圖

--- app/src/main/java/com/showme/AccountHelper/ui/home/HomeFragment.kt
+++ app/src/main/java/com/showme/AccountHelper/ui/home/HomeFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.showme.AccountHelper.databinding.FragmentHomeBinding
@@ -28,9 +27,9 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
+//        val textView: TextView = binding.topView
         homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+//            textView.text = it
         }
         return root
     }
